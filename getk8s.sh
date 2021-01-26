aed17c6027e1dfea18a58fe4c5cf11eefe84d874
Luigi Molinaro <luigi.molinaro@neen.it>
2021-01-26 08:55:36 +0100

    New Version

--- getk8s.sh
+++ getk8s.sh
@@ -4,8 +4,8 @@
 # https://github.com/itaysk/kubectl-neat correctly installed 
 # https://github.com/kubernetes-sigs/krew correctly installed
  
-for n in $(kubectl neat get -o=name pvc,configmap,serviceaccount,secret,ingress,service,deployment,statefulset,hpa,job,cronjob)
+for n in $(kubectl get -o=name pvc,configmap,serviceaccount,secret,ingress,service,deployment,statefulset,hpa,job,cronjob)
 do
     mkdir -p $(dirname $n)
-    kubectl get -o=yaml $n > $n.yaml
+    kubectl neat get -o=yaml $n > $n.yaml
 done
